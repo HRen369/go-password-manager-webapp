61f0d2f3bdc82d44a2f53a548c685263fe164905
hrendon369 <hrendon369@gmail.com>
2025-08-15 19:53:27 -0700

    added general skeleton of console app

--- main.go
+++ main.go
@@ -2,8 +2,48 @@ package main
 
 import "fmt"
 
+func viewLogins() {
+	fmt.Println("-- View Logins -- ")
+	fmt.Println(("[1] Github: HTest ngh4rnv.e#fku"))
+}
+
+func addLogins() {
+	fmt.Println("-- Add Logins --")
+	fmt.Println("Site Name: ")
+	fmt.Println("Username:  ")
+	fmt.Println("Password:  ")
+
+}
+
+func deleteLogins() {
+	fmt.Println("-- Delete Logins --")
+	fmt.Println(("[1] Github: HTest ngh4rnv.e#fku"))
+	fmt.Println("---++")
+}
+
 func consoleApp() {
-	fmt.Println("-- Go Password Manager Console App")
+	var running bool = true
+
+	for running {
+		fmt.Println("-- Go Password Manager Console App")
+		fmt.Println("[1] View Login Credentials")
+		fmt.Println("[2] Add Login Credentials")
+		fmt.Println("[3] Delete Login Credentials")
+
+		var option string
+		fmt.Scan(&option)
+
+		if option == "1" {
+			viewLogins()
+		} else if option == "2" {
+			addLogins()
+		} else if option == "3" {
+			deleteLogins()
+		} else if option == "q" {
+			running = false
+		}
+	}
+
 }
 
 func main() {
