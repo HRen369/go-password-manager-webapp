27df5a702a0fca53d1af072efbe95291392db751
hrendon369 <hrendon369@gmail.com>
2025-08-15 21:22:47 -0700

    added delete function

--- main.go
+++ main.go
@@ -1,27 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-type credential struct {
+type Credential struct {
 	siteName            string
 	username            string
 	passwordUnencrypted string
 }
 
-var testUser1 = credential{
+// Later will be converted to a file; Inside code for now
+var testUser1 = Credential{
 	siteName:            "Github",
 	username:            "HTest",
 	passwordUnencrypted: "ngh4rnv.e#fku",
 }
 
-siteCredentials := []credential{}
+var siteCredentials = []Credential{testUser1}
 
+// Main Logic for Console App
+func getSite() int {
+	var option string
+	fmt.Scan(&option)
+
+	choiceInt, err := strconv.Atoi(option)
+
+	if err != nil || choiceInt >= len(siteCredentials) {
+		return -1
+	}
+
+	return choiceInt
+}
 
 func viewLogins() {
 	fmt.Println("-- View Logins -- ")
 
 	for i, userCredential := range siteCredentials {
-		fmt.Printf("[%d] %s %s %s\n", i, userCredential.siteName, userCredential.username, userCredential.passwordUnencrypted)
+		fmt.Printf("[%d] %s %s %s\n", i+1, userCredential.siteName, userCredential.username, userCredential.passwordUnencrypted)
 	}
 }
 
@@ -31,11 +48,18 @@ func addLogins() {
 	fmt.Println("Username:  ")
 	fmt.Println("Password:  ")
 
+	fmt.Println("---++")
 }
 
 func deleteLogins() {
 	fmt.Println("-- Delete Logins --")
-	fmt.Println(("[1] Github: HTest ngh4rnv.e#fku"))
+	viewLogins()
+
+	var indexToDel int = getSite()
+	if indexToDel > -1 {
+		siteCredentials = append(siteCredentials[:indexToDel], siteCredentials[indexToDel+1:]...)
+	}
+
 	fmt.Println("---++")
 }
 
@@ -57,7 +81,7 @@ func consoleApp() {
 			addLogins()
 		} else if option == "3" {
 			deleteLogins()
-		} else if option == "q" {
+		} else if option == "7" {
 			running = false
 		}
 	}
